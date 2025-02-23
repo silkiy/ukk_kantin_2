f4ba0091dab8526bf7b2c1d56a59d4a547424650
silkiy <wildanmilk@gmail.com>
2025-02-23 14:49:02 +0700

    feat (frontend) : add home page

--- lib/pages/user/home_page_student.dart
+++ lib/pages/user/home_page_student.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ukk_kantin_menyok/components/bottom_nav_bar.dart';
+import 'package:ukk_kantin_menyok/components/search_bar.dart';
+
+import '../../components/stand_card.dart';
 
 class HomePageStudent extends StatefulWidget {
   const HomePageStudent({super.key});
@@ -8,8 +13,91 @@ class HomePageStudent extends StatefulWidget {
 }
 
 class _HomePageStudentState extends State<HomePageStudent> {
+  final List<Map<String, String>> stands = [
+    {
+      "name": "Stand A",
+      "img": "assets/images/kantin.jpeg",
+      "jamOperational": "09:00 - 12:00",
+      "buka": "setiap hari",
+    },
+    {
+      "name": "Stand B",
+      "img": "assets/images/kantin.jpeg",
+      "jamOperational": "09:00 - 12:00",
+      "buka": "setiap hari",
+    },
+    {
+      "name": "Stand C",
+      "img": "assets/images/kantin.jpeg",
+      "jamOperational": "09:00 - 12:00",
+      "buka": "setiap hari",
+    },
+    {
+      "name": "Stand D",
+      "img": "assets/images/kantin.jpeg",
+      "jamOperational": "09:00 - 12:00",
+      "buka": "setiap hari",
+    },
+    {
+      "name": "Stand A",
+      "img": "assets/images/kantin.jpeg",
+      "jamOperational": "09:00 - 12:00",
+      "buka": "setiap hari",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(
+            vertical: MediaQuery.of(context).size.height * 0.05,
+            horizontal: MediaQuery.of(context).size.width * 0.03,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SearchBarComponents(),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+              Image.asset(
+                "assets/images/banner1.webp",
+                height: 166,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+              Text(
+                "Pengumuman Hari Ini : ",
+                style: GoogleFonts.poppins(
+                  fontSize: MediaQuery.of(context).size.width * 0.035,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.15),
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  physics: AlwaysScrollableScrollPhysics(),
+                  itemCount: stands.length,
+                  separatorBuilder: (context, index) => SizedBox(height: 10),
+                  itemBuilder: (context, index) {
+                    return StandCard(
+                      name: stands[index]["name"]!,
+                      img: stands[index]["img"]!,
+                      buka: stands[index]["buka"]!,
+                      jamOperational: stands[index]["jamOperational"]!,
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavBar(selectedItem: 0),
+    );
   }
 }
