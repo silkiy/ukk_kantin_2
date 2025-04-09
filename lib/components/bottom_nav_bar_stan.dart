5c3bebcb6130b9d17de1b2ba0d6b45d43745c103
Wildan Silki <wildan.silki@merkleinnovation.co.id>
2025-04-09 10:07:06 +0700

    feat (frontend) : add home page stan

---
+++ lib/components/bottom_nav_bar_stan.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: unused_field
+
+import 'package:flutter/material.dart';
+import 'package:ukk_kantin_menyok/pages/user/activity_page.dart';
+import 'package:ukk_kantin_menyok/pages/user/profile_page.dart';
+
+import '../pages/stan/home_page_stan.dart';
+
+
+class BottomNavBarStan extends StatefulWidget {
+  final int selectedItem;
+  const BottomNavBarStan({super.key, required this.selectedItem});
+
+  @override
+  State<BottomNavBarStan> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNavBarStan> {
+  int _currentIndex = 0;
+
+  void changeSelectedNavBar(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+    if (index == 0) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const HomePageStan()),
+      );
+    } else if (index == 1) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const ActivityPage()),
+      );
+    } else if (index == 2) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const ProfilePage()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      showUnselectedLabels: true,
+      backgroundColor: const Color.fromRGBO(243, 244, 248, 1),
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.shopping_bag),
+          label: 'Transaksi',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle_outlined),
+          label: 'Profile',
+        ),
+      ],
+      selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.black26,
+      currentIndex: widget.selectedItem,
+      onTap: changeSelectedNavBar,
+    );
+  }
+}
--- lib/pages/stan/home_page_stan.dart
+++ lib/pages/stan/home_page_stan.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ukk_kantin_menyok/components/bottom_nav_bar_stan.dart';
 
 class HomePageStan extends StatefulWidget {
   const HomePageStan({super.key});
@@ -11,7 +12,110 @@ class _HomePageStanState extends State<HomePageStan> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Text(
+                    "HALO, PENGELOLA ! ",
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width * 0.055,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Edit Profil Kantin Anda !",
+                          style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.width * 0.04,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/profil_kantin.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    Row(
+                      children: [
+                        Text(
+                          "Sesuikan Menuu yang Anda Jual !",
+                          style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.width * 0.04,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/simpan_menu.webp"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    Row(
+                      children: [
+                        Text(
+                          "Tambakan Diskon Untuk Menu Anda !",
+                          style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.width * 0.04,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/sale.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavBarStan(selectedItem: 0),
     );
   }
 }
